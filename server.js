074b3f78fd75c9a3ef4215bac14a8e0d073abed7
yang seung jae <ysjj6105@naver.com>
2023-06-09 16:49:35 +0900

    add session

--- server.js
+++ server.js
@@ -4,10 +4,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const mongodbstore = require('connect-mongodb-session')(session);
+
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
 const app = express();
+const mongodbUri = 'mongodb+srv://user1:keWCTShXDqb0x84U@cluster0.h9b9gd7.mongodb.net/shop';
+const store = new mongodbstore({
+    uri: mongodbUri,
+    collection: 'sessions'
+})
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -18,15 +25,23 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+    session({
+        secret: 'my secret',
+        resave: false,
+        saveUninitialized: false,
+        store: store
+    })
+);
 
 app.use((req, res, next) => {
     User
-    .findById('646f1c8c5954bb471d4108f4')
-    .then(user => {
-        req.user = user
-        next();
-    })
-    .catch(err => console.log(err));
+        .findById('646f1c8c5954bb471d4108f4')
+        .then(user => {
+            req.user = user
+            next();
+        })
+        .catch(err => console.log(err));
 });
 
 app.use('/admin', adminRoutes);
@@ -36,22 +51,22 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-.connect('mongodb+srv://user1:keWCTShXDqb0x84U@cluster0.h9b9gd7.mongodb.net/shop?retryWrites=true&w=majority')
-.then(result => {
-    User.findOne().then(user => { 
-        if( !user ) {
-            const user = new User({
-                name: 'ysj',
-                email: 'ysj@test.com',
-                cart: {
-                    item: []
-                }
-            });
-            user.save();
-        }
+    .connect(mongodbUri)
+    .then(result => {
+        User.findOne().then(user => {
+            if (!user) {
+                const user = new User({
+                    name: 'ysj',
+                    email: 'ysj@test.com',
+                    cart: {
+                        item: []
+                    }
+                });
+                user.save();
+            }
+        })
+        app.listen(3000)
     })
-    app.listen(3000)
-})
-.catch(err => {
-    console.log(err)
-});
+    .catch(err => {
+        console.log(err)
+    });
