517e42e382b4fe386cb0aea2fc84c79105edbe5a
yang seung jae <ysjj6105@naver.com>
2023-06-24 16:04:12 +0900

    remove cookies

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,6 @@
-const user = require("../models/user");
+const User = require("../models/user");
 
 exports.getLogin = (req, res, next) => {
-    // const isLoggedIn = req.get('Cookie')
-    //     .split(';')[0]
-    //     .trim()
-    //     .split('=')[1];
-    console.log(req.session.isLoggedIn)
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'login',
@@ -14,6 +9,20 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-    req.session.isLoggedIn = true;
-    res.redirect('/');
+    User.findById('646f1c8c5954bb471d4108f4')
+    .then(user => {
+        req.session.isLoggedIn = true;
+        req.session.user = user;
+        res.redirect('/');
+    })
+    .catch(err => {
+        console.log(err)
+    })
+};
+
+exports.postLogout = (req, res, next) => {
+    req.session.destroy(err => {
+        console.log(err);
+        res.redirect('/');
+    });
 };
--- server.js
+++ server.js
@@ -34,16 +34,6 @@ app.use(
     })
 );
 
-app.use((req, res, next) => {
-    User
-        .findById('646f1c8c5954bb471d4108f4')
-        .then(user => {
-            req.user = user
-            next();
-        })
-        .catch(err => console.log(err));
-});
-
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
