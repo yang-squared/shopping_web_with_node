fc4f376a8bf83b1a4c0040ecaab4a1d9ce0647a6
yang seung jae <ysjj6105@naver.com>
2023-08-09 17:01:33 +0900

    restAPI 연습

--- restAPI/controller/feed.js
+++ restAPI/controller/feed.js
@@ -2,7 +2,7 @@ exports.getPosts = (req, res, next) => {
     res.status(200).json ({
         post: [{
             title: "test",
-            decription: "test"
+            description: "test description"
         }]
     });
 };
