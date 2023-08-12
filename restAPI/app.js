c6bd28bb1636699cfa689499d9e0d5374e3cfce4
yang seung jae <ysjj6105@naver.com>
2023-08-12 15:51:33 +0900

    CORS 오류 해결

--- restAPI/app.js
+++ restAPI/app.js
@@ -8,6 +8,13 @@ const app = express();
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Constrol-Allow-Origin","*");
+    res.setHeader("Access-Constrol-Allow-Method", 'OPTION, GET, POST, PUT, PATH, DELETE');
+    res.setHeader("Access-Constrol-Allow-Headers","Content-Type,Authhorization");
+    next();
+})
+
 app.use('/feed', feedRoutes);
 
 app.listen(8080);
