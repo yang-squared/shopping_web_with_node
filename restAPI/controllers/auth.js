af999b6650dc742e0cdd0d73bef017f30ac1270e
yang seung jae <ysjj6105@naver.com>
2023-08-20 15:42:34 +0900

    로그인 토큰 추가

--- restAPI/controllers/auth.js
+++ restAPI/controllers/auth.js
@@ -1,5 +1,6 @@
 const { validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -39,7 +40,7 @@ exports.signup = (req, res, next) => {
 exports.login = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
-
+    let loadedUser;
     User
         .findOne({ email: email })
         .then(user => {
@@ -48,7 +49,7 @@ exports.login = (req, res, next) => {
                 error.statusCode = 401;
                 throw error;
             }
-            loadUser = user;
+            loadedUser = user;
             return bcrypt.compare(password, user.password)
         })
         .then(isEqual => {
@@ -57,12 +58,20 @@ exports.login = (req, res, next) => {
                 error.statusCode = 401;
                 return error;
             }
-            
+            const token = jwt.sign(
+            { 
+                email: loadedUser.email, 
+                userId: loadedUser._id.toString() 
+            }, 
+            'somesupersecretsecret', 
+            { expiresIn: '1h'}
+            );
+            res.status(200).json({ token: token, userID: loadedUser._id.toString() })
         })
         .catch(err => {
             if (!err.statusCode) {
                 err.statusCode = 500;
             }
             next(err);
-        })
-}   
+        });
+};
