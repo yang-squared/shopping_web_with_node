70bfd3f6fb146741d5c04a0b2abf448d2617788f
yang seung jae <ysjj6105@naver.com>
2023-08-23 17:03:23 +0900

    비동기 작업 async await형식

--- restAPI/controllers/auth.js
+++ restAPI/controllers/auth.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
-exports.signup = (req, res, next) => {
+exports.signup = async (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         const error = new Error('validion failed');
@@ -15,26 +15,23 @@ exports.signup = (req, res, next) => {
     const email = req.body.email;
     const name = req.body.name;
     const password = req.body.password;
-    bcrypt
-        .hash(password, 12)
-        .then(hashedPw => {
-            const user = new User({
-                email: email,
-                password: hashedPw,
-                name: name
-            });
-            return user.save();
-        })
-        .then(result => {
-            res.status(201)
-                .json({ message: 'signup success', userId: result._id })
-        })
-        .catch(err => {
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err);
-        })
+    try {
+        const hashedPw = await bcrypt.hash(password, 12);
+
+        const user = new User({
+            email: email,
+            password: hashedPw,
+            name: name
+        });
+        const result = await user.save();
+        res.status(201)
+            .json({ message: 'signup success', userId: result._id });
+    } catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    }
 }
 
 exports.login = (req, res, next) => {
@@ -59,12 +56,12 @@ exports.login = (req, res, next) => {
                 return error;
             }
             const token = jwt.sign(
-            { 
-                email: loadedUser.email, 
-                userId: loadedUser._id.toString() 
-            }, 
-            'somesupersecretsecret', 
-            { expiresIn: '1h'}
+                {
+                    email: loadedUser.email,
+                    userId: loadedUser._id.toString()
+                },
+                'somesupersecretsecret',
+                { expiresIn: '1h' }
             );
             res.status(200).json({ token: token, userId: loadedUser._id.toString() })
         })
